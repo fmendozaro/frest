a64a0e8564d0afb93ca86aadb9b91fac40b2b359
fer <fernandoza@gmail.com>
2018-06-12 11:23:14 -0500

    fix: manifest file is responding and moved the sw.js to the root of the project

--- sw.js
+++ sw.js
@@ -1,7 +1,5 @@
 "use strict";
 
-// import idb from 'main-idb.js';
-
 let staticCacheName = 'frest-v1';
 let contentImgsCache = 'frest-content-imgs';
 let allCaches = [
@@ -12,9 +10,8 @@ let allCaches = [
 self.addEventListener('install', event => {
 
     caches.open(staticCacheName).then( cache => {
-        console.log('Opened cache');
         return cache.addAll([
-            '/',
+            '.',
             '/index.html',
             '/restaurant.html',
             '/css/styles.css',
@@ -32,7 +29,6 @@ self.addEventListener('install', event => {
             '/img/favicon.ico',
             '/dist/index_bundle.js',
             '/dist/info_bundle.js',
-            '/dist/sw_bundle.js',
             '/vendor/sweetalert.min.js',
             '/vendor/jquery-3.3.1.min.js',
             '/vendor/toastr.min.css',
@@ -60,70 +56,17 @@ self.addEventListener('activate', function (event) {
 
 self.addEventListener('fetch', event => {
     console.log('fetched');
-    let requestUrl = new URL(event.request.url);
-
-    console.log(event);
-    console.log(event.request);
-    console.log(event.response);
-    console.log(requestUrl);
-
-    // if (requestUrl.origin === location.origin) {
-    //     if (requestUrl.pathname === '/') {
-    //         event.respondWith(caches.match('/index.html'));
-    //         return;
-    //     }
-    // }
-    //
-
-
-    // if (requestUrl.origin === location.origin) {
-    //     console.log(requestUrl.pathname);
-    //     if (requestUrl.pathname.startsWith('/img/')) {
-    //         event.respondWith(servePhoto(event.request));
-    //         return;
-    //     }
-    //
-    //     if (requestUrl.pathname.match('/')){
-    //         event.respondWith(caches.match('/'));
-    //         return;
-    //     }
-    // }
 
     event.respondWith(
-        caches.match(event.request).then( response => {
-
-            toastr.error('Unable to connect');
-
-            if(response){
-                return response;
-            }
-
-            let fetchRequest = event.request.clone();
-
-            return fetch(fetchRequest).then( response => {
-                console.log(response);
-
-                if(!response || response.status !== 200 || response.type !== 'basic') {
-                    return response;
+        caches.match(event.request)
+            .then(function(response) {
+                    // Cache hit - return response
+                    if (response) {
+                        return response;
+                    }
+                    return fetch(event.request);
                 }
-
-                if (response.status === 404) {
-                    return fetch('/img/404-gif-8.gif')
-                }
-
-                let responseToCache = response.clone();
-
-                caches.open(staticCacheName).then( cache => {
-                    cache.put(event.request, responseToCache);
-                });
-
-                return response;
-
-            }).catch( () => {
-                return new Response("Something totally failed");
-            });
-
-        })
+            )
     );
 
 });
