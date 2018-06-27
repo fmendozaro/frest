43303302ce656713bff24cd1432aa1ada22ea3e0
Fernando <fernandoza@gmail.com>
2018-06-26 23:02:11 -0500

    fix: bug in map

--- js/main.js
+++ js/main.js
@@ -81,7 +81,9 @@ let initMap = () => {
     };
 
     self.map = new L.map('map').on('load', function(e) {
-        document.getElementById('map-placeholder').className = 'fade-out';
+        let ph = document.getElementById('map-placeholder');
+        ph.className = 'fade-out';
+        ph.style.display = 'none';
     }).setView(loc, 12);
 
     L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
